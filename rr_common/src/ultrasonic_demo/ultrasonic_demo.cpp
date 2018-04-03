5055cfcbc3d20cfc16c6f58b498be47c2de792ad
Btdubs <Brianco1998@gmail.com>
2018-04-02 20:42:34 -0400

    Reworked iterative method

--- rr_common/src/ultrasonic_demo/ultrasonic_demo.cpp
+++ rr_common/src/ultrasonic_demo/ultrasonic_demo.cpp
@@ -12,13 +12,17 @@
 double steering = 0.0;
 
 
-void ultrasonicCallBack(const sensor_msgs::PointCloud2ConstPtr& obstacles) {
-  pcl::PointCloud2 map = *obstacles; this is not what to do probz
+void ultrasonicCallBack(const sensor_msgs::PointCloud2ConstPtr& cloud_msg) {
+  pcl::PointCloud2<pcl::PointXYZ> cloud;;
+  pcl::fromROSMsg( *cloud_msg, cloud);
 
-  float minPoint = map(0,0);
+  //#TODO: possibly trasnform to car frame coordinates here
+
+  float minPoint = cloud[0];
   float minDistance = pcl::euclideanDistance(minPoint, PointXYZ(0,0,0));
 
-  for (pcl::PointXYZ point : map) { user iterator
+  for (int i = 0; i < cloud.size(); i++ ) {
+    pcl::PointXYZ point = cloud[i];
     float dist = pcl::euclideanDistance(point, PointXYZ(0,0,0));
     if (dist < minDistance) {
       minPoint = point;
